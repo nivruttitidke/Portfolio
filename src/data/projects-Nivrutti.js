f543f88a673c12443c40628d06657a912ac041f1
Nivrutti Tidke <nivrutitidke108@gmail.com>
2026-08-10 21:19:24 +0530

    Add new data

---
+++ src/data/projects-Nivrutti.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+  id: 1,
+
+  title: "ChatNest — Real-Time Chat & Video Calling Platform",
+
+  description:
+    "Production-ready real-time communication platform built using the MERN stack. Enables secure messaging, friend requests, notifications, and one-click video calling using Stream APIs. Designed with scalable architecture, JWT authentication, and deployed on cloud infrastructure (Vercel and Render).",
+
+  image: "/project1.png",
+
+  tech: [
+    "React",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "Stream Chat API",
+    "Stream Video SDK",
+    "JWT Authentication",
+    "Tailwind CSS",
+    "React Query",
+    "Axios"
+  ],
+
+  highlights: [
+    "Implemented real-time messaging and video calling using Stream APIs",
+    "Built secure authentication system with JWT and HTTP-only cookies",
+    "Handled production deployment with CORS, cookies, and environment configuration",
+    "Deployed frontend on Vercel and backend on Render"
+  ],
+
+  category: "Full Stack / Real-Time Application",
+
+  live: "https://chatnest-eta.vercel.app",
+
+  github: "https://github.com/nivruttitidke/Chatnest"
+},
+  {
+    id: 2,
+    title: " Skillmate-Ai",
+    description: "Multilingual MERN chatbot for interview preparation (Hindi, English, Marathi) Integrated Groq API for fast AI response generation",
+    image: "/project2.png",
+    tech: ["React", "Node.js", "Express", "MongoDB","JWT auth"],
+    live: "https://skill-mate-ai-pink.vercel.app",
+    github: "https://github.com/nivruttitidke/SkillMate-AI.git",
+  },
+  {
+    id: 3,
+    title: "E-commerce UI",
+    description: "Frontend UI with clean animations and responsive layout.",
+    image: "/project3.png",
+    tech: ["React", "Tailwind", "Framer Motion"],
+    live: "https://your-live.com",
+    github: "",
+  },
+];
